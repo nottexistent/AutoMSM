8255b5c761e7f4d110b2f3f3de45606c0862f7ef
nottexistent <117927470+nottexistent@users.noreply.github.com>
2022-11-10 10:23:32 -0500

    Add files via upload
    Base (Not at all finished just stuff imported)

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+Author: Jordyn Kuhn
+Date: 11.10.22
+Ver: 0.1
+
+-------GOAL-------
+Create a program that auto-clicks auto breed in msm
+and zaps them to my statues when its done breeding
+"""
+
+import pyautogui
+import win32gui
+
+def get_Screen():
+    pyautogui.position()
+
+def breed():
+
+
+
+if __name__ == '__main__':
+    breed()
